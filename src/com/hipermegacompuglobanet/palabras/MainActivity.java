12a9cbef72ac443e0c815641287c9279ddfe4b20
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2014-04-28 10:10:58 +0200

    Search query correctly sent to MainActivity in an intent.

--- src/com/hipermegacompuglobanet/palabras/MainActivity.java
+++ src/com/hipermegacompuglobanet/palabras/MainActivity.java
@@ -1,22 +1,95 @@
 package com.hipermegacompuglobanet.palabras;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private final String TAG = MainActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate()");
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		// setContentView(R.layout.activity_main);
+		// String hello = getResources().getString(R.string.hello_world);
+
+		// Get the intent, verify the action and get the query
+		Intent intent = getIntent();
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			Log.d(TAG, "onCreate().Searching: " + query);
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		MenuItem searchItem = menu.findItem(R.id.action_search);
+
+		// Get the SearchView and set the searchable configuration
+		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		SearchView searchView = (SearchView) menu.findItem(R.id.action_search)
+				.getActionView();
+
+		// Assumes current activity is the searchable activity
+		searchView.setSearchableInfo(searchManager
+				.getSearchableInfo(getComponentName()));
+		searchView.setIconifiedByDefault(false); // Do not iconify the widget;
+													// expand it by default
+
+		// Configure the search info and add any event listeners
+		// searchView.setOnQueryTextListener(new OnQueryTextListener() {
+		//
+		// @Override
+		// public boolean onQueryTextSubmit(String query) {
+		//
+		//
+		// return true;
+		// }
+		//
+		// @Override
+		// public boolean onQueryTextChange(String newText) {
+		// return false;
+		// }
+		// });
+
+		return super.onCreateOptionsMenu(menu);
 	}
 
+	@Override
+	protected void onNewIntent(Intent intent) {
+		Log.d(TAG, "onNewIntent()");
+		setIntent(intent);
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			Log.d(TAG, "onNewIntent().Searching: " + query);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d(TAG, "onOptionsItemSelected(item = " + item.toString());
+		switch (item.getItemId()) {
+		case R.id.action_search:
+			(Toast.makeText(getApplicationContext(), "Search selected",
+					Toast.LENGTH_SHORT)).show();
+			break;
+		case R.id.action_about:
+		case R.id.action_settings:
+		default:
+			(Toast.makeText(getApplicationContext(), "Not implemented",
+					Toast.LENGTH_SHORT)).show();
+			break;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 }
